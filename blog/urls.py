c0de2f485e04962c33a76e35237e037b401ee183
lukaszmaterla <lukasz.materla91@gmail.com>
2019-04-12 18:50:13 +0200

    Added article create view with create form

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from .views import (
     ArticleListView,
-    ArticleDetailView
+    ArticleDetailView,
+    ArticleCreateView
 )
 
 app_name = 'articles'
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article-list'),
-    # path('create/', product_create_view, name='product-list'),
-    path('<int:pk>/', ArticleDetailView.as_view(), name='product-detail'),
+    path('create/', ArticleCreateView.as_view(), name='article-list'),
+    path('<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
     # path('<int:id>/update/', product_update_view, name='product-update'),
     # path('<int:id>/delete/', product_delete_view, name='product-delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ from django.views.generic import (
 )
 # Create your views here.
 from .models import Article
+from .forms import ArticleModelForm
 
 
 class ArticleListView(ListView):
@@ -16,8 +17,14 @@ class ArticleListView(ListView):
 
 
 class ArticleDetailView(DetailView):
-    queryset = Article.objects.all() # blog/<modelname>_list.html
+    # queryset = Article.objects.all() # blog/<modelname>_list.html
 
     def get_object(self, queryset=None):
         id_ = self.kwargs.get('id')
         return get_object_or_404(Article, id=id_)
+
+
+class ArticleCreateView(CreateView):
+    template_name = 'blog/article_create.html'
+    queryset = Article.objects.all() # blog/<modelname>_list.html
+    form_class = ArticleModelForm
