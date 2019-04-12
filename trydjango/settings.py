1ac2206d547857a13a34b9593ac412487d9aa81a
lukaszmaterla <lukasz.materla91@gmail.com>
2019-04-12 19:50:11 +0200

    added courses config to the project

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
 
     'products',
     'pages',
-    'blog'
+    'blog',
+    'courses'
 ]
 
 MIDDLEWARE = [
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('about/', about_view),
     path('products/', include('products.urls')),
     path('blog/', include('blog.urls')),
+    path('courses/', include('courses.urls')),
     path('admin/', admin.site.urls),
 ]
